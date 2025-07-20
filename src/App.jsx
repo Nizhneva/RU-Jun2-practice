40b8f02c1fc1805c9cb6b6a8d7093ac839fae013
Нижнева Евгения Владимировна <nizhneva@mts.ru>
2025-07-20 18:31:45 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const SDiv = styled.div`
+    text-align: center;
+`;
+
+export const App = () => {
+    return (
+        <div>
+            <SDiv>
+                <i className="fa fa-address-book" aria-hidden="true"></i>
+                {"   "}
+                bla bla 
+            </SDiv>
+
+            <i className="fa fa-camera-retro"></i> fa-camera-retro
+        </div>
+    )
+}
